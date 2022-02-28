baeef94122bbddd4c256809551eb7d2124521a5d
Salah93200 <lahmadis@yahoo.com>
2022-02-28 22:29:52 +0100

    history

--- basic_cnn.py
+++ basic_cnn.py
@@ -25,3 +25,23 @@ model.add(Flatten())
 
 model.add(Dense(128, activation='relu'))
 model.add(Dense(train['category_id'].unique().shape[0], activation = 'softmax'))
+
+model.fit_generator(train_datagen.flow_from_dataframe(dataframe=train,
+                                                      directory=train_dir,
+                                                      x_col="file_name",
+                                                      y_col="category_id",
+                                                      target_size=(299, 299),
+                                                      batch_size=32,
+                                                     class_mode= "categorical"),
+                    validation_data=train_datagen.flow_from_dataframe(
+                        dataframe=test,
+                        directory= train_dir,
+                        x_col="file_name",
+                        y_col="category_id",
+                        target_size=(299, 299),
+                        batch_size=32,
+                    class_mode="categorical"),
+                    epochs=20,
+                    steps_per_epoch=len(train)//32,
+                    validation_steps=len(test)//32,
+                    verbose=2)
